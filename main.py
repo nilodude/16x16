9ffec53159ad47c1a4aa81001e2f1c57e230e4da
nilodude <alphone@protonmail.com>
2024-03-27 15:36:06 +0100

    must integrate ADC instant measure, etc

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ adcvalue = 0
 pixels = Neopixel(16*16, 0, 2, "GRB") 
 
 val = 0
+lastBr = 0
 dir = True
 
 def tick(timer):
@@ -16,13 +17,14 @@ def tick(timer):
     led.toggle()
 
 tim.init(freq=1, mode=Timer.PERIODIC, callback=tick)
+brDecay = 50
 
 while(True):
     adcvalue = adc.read_u16()
     
-    print(int(2000*(adcvalue/65000)))
+    adcc = int(3000*(adcvalue/65000))
     
-    adcc = int(2000*(adcvalue/65000))
+    print(adcc)
     
     val = val + (40*(1 if dir else -1))
     
@@ -32,8 +34,13 @@ while(True):
         dir = True
     
 #     follow ADC
-    br = int(adcc/50)
-#     pixels.brightnessvalue = br   
+    br = 255 if adcc > 255 else int(adcc)
+    br = 1 if adcc < 1 else int(adcc)
+    
+    if(br < lastBr):
+        br = br - brDecay
+        
+    pixels.brightnessvalue = br if br > 5 else 5  
     
 #     up and down
     r=int(val/80)
@@ -42,10 +49,14 @@ while(True):
     rgbw1 = color
     rgbw2 = (56,20+0.1*r, 8-0.1*r)
     
-    pixels.set_pixel_line_gradient(0, 255, rgbw1, rgbw2) # display parpadea cuando hay que llegar a muchos pixeles, se nota latencia
+#     pixels.set_pixel_line_gradient(0, 255, rgbw1, rgbw2) # display parpadea cuando hay que llegar a muchos pixeles, se nota latencia
+    
+    color = pixels.colorHSV(br, 100, 100)
+    pixels.fill(color)
     
 #     hay que investigar porqué el color (aprox) blanco se consigue con (r,g,b)=(94,60,255) en la matriz 16x16
 #     con el r=94, g=60, y bajando el azul de 255 se consigue blanco más cálido, pero al bajar el azul el verde hay que bajarlo un poco tambien
 #     pixels.fill((94,50,100))
 #     pixels.fill((94,60,255))
-    pixels.show()    
+    pixels.show()
+    lastBr = br
