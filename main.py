1ae500fb22c66e8dd71b2c33e8a4c2947ed9d9ac
nilodude <alphone@protonmail.com>
2024-03-30 18:37:07 +0100

    proper dB measurement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from machine import Pin, Timer
 from neopixel import Neopixel
+import math
 
 led = Pin("LED", Pin.OUT)
 tim = Timer()
@@ -9,7 +10,6 @@ adcvalue = 0
 pixels = Neopixel(16*16, 0, 2, "GRB") 
 
 val = 0
-lastBr = 0
 dir = True
 
 def tick(timer):
@@ -17,30 +17,43 @@ def tick(timer):
     led.toggle()
 
 tim.init(freq=1, mode=Timer.PERIODIC, callback=tick)
-brDecay = 50
+
+def measure():
+    raw = 0
+    numsamples = 700
+    rms = 0
+    for i in range(numsamples):
+        raw = 65535 - adc.read_u16()
+        amp = abs(raw - 32767)
+        rms = rms + (amp*amp)
+
+    rms = rms/numsamples
+    dB = 20.0 * math.log10(math.sqrt(rms) / 32767);
+#     print(dB)
+    return dB
 
 while(True):
     adcvalue = adc.read_u16()
     
-    adcc = int(3000*(adcvalue/65000))
+#     print(int(2000*(adcvalue/65000)))
     
-    print(adcc)
+#     adcc = int(2000*(adcvalue/65000))
     
+    
+#     follow ADC
+    br= abs(measure() *1000)
+    
+    
+    print(br)
+    pixels.brightnessvalue = br 
+    
+        
     val = val + (40*(1 if dir else -1))
     
     if val >= 2000:
         dir = False
     elif val <= 0:
         dir = True
-    
-#     follow ADC
-    br = 255 if adcc > 255 else int(adcc)
-    br = 1 if adcc < 1 else int(adcc)
-    
-    if(br < lastBr):
-        br = br - brDecay
-        
-    pixels.brightnessvalue = br if br > 5 else 5  
     
 #     up and down
     r=int(val/80)
@@ -51,12 +64,8 @@ while(True):
     
 #     pixels.set_pixel_line_gradient(0, 255, rgbw1, rgbw2) # display parpadea cuando hay que llegar a muchos pixeles, se nota latencia
     
-    color = pixels.colorHSV(br, 100, 100)
-    pixels.fill(color)
-    
 #     hay que investigar porqué el color (aprox) blanco se consigue con (r,g,b)=(94,60,255) en la matriz 16x16
 #     con el r=94, g=60, y bajando el azul de 255 se consigue blanco más cálido, pero al bajar el azul el verde hay que bajarlo un poco tambien
-#     pixels.fill((94,50,100))
+    pixels.fill((94,50,100))
 #     pixels.fill((94,60,255))
-    pixels.show()
-    lastBr = br
+    pixels.show()    
