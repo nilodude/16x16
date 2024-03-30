ce7219455a9b4dda19f57653d0c61ad684a6f7e2
nilodude <alphone@protonmail.com>
2024-03-30 19:32:15 +0100

    need to repair minijack

--- main.py
+++ main.py
@@ -26,26 +26,17 @@ def measure():
         raw = 65535 - adc.read_u16()
         amp = abs(raw - 32767)
         rms = rms + (amp*amp)
-
     rms = rms/numsamples
     dB = 20.0 * math.log10(math.sqrt(rms) / 32767);
-#     print(dB)
     return dB
 
 while(True):
-    adcvalue = adc.read_u16()
-    
-#     print(int(2000*(adcvalue/65000)))
-    
-#     adcc = int(2000*(adcvalue/65000))
-    
-    
+
 #     follow ADC
     br= abs(measure() *1000)
     
-    
     print(br)
-    pixels.brightnessvalue = br 
+#     pixels.brightnessvalue = br 
     
         
     val = val + (40*(1 if dir else -1))
@@ -60,13 +51,13 @@ while(True):
 
     color = (3*4+r, 4+r, 30-r)
     rgbw1 = color
-    rgbw2 = (56,20+0.1*r, 8-0.1*r)
+    rgbw2 = (56,20+0.3*r, 8-0.3*r)
     
-#     pixels.set_pixel_line_gradient(0, 255, rgbw1, rgbw2) # display parpadea cuando hay que llegar a muchos pixeles, se nota latencia
+    pixels.set_pixel_line_gradient(0, 255, rgbw1, rgbw2) # display parpadea cuando hay que llegar a muchos pixeles, se nota latencia
     
 #     hay que investigar porqué el color (aprox) blanco se consigue con (r,g,b)=(94,60,255) en la matriz 16x16
 #     con el r=94, g=60, y bajando el azul de 255 se consigue blanco más cálido, pero al bajar el azul el verde hay que bajarlo un poco tambien
 #     pixels.fill((94,50,100))
 #     pixels.fill((94,60,255))
-    pixels.fill(rgbw1)
+#     pixels.fill(rgbw1)  # junto con brightnessvalue cambiando segun ADC
     pixels.show()    
