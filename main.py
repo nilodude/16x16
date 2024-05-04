9957ebf4630b377825138e14eb3bf996f44c21f8
alermat <alfonso.lerma@digital55.com>
2024-05-04 18:08:31 +0200

    some fixes

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ tim = Timer()
 
 adc = machine.ADC(28)
 adcvalue = 0
-pixels = Neopixel(16*16, 0, 2, "GRB") 
+pixels = Neopixel(16*16 + 180, 0, 2, "GRB") 
 
 val = 0
-dVal = 15
+dVal = 5
 dir = True
 
 def tick(timer):
@@ -20,10 +20,10 @@ def tick(timer):
 
 tim.init(freq=1, mode=Timer.PERIODIC, callback=tick)
 
-def measure():
+def measure(mean=500):
     dB=0
     raw = 0
-    numsamples = 700
+    numsamples = mean
     rms = 0
     for i in range(numsamples):
         raw = 65535 - adc.read_u16()
@@ -31,39 +31,44 @@ def measure():
         rms = rms + (amp*amp)
     rms = rms/numsamples
     dB = 20.0 * math.log10(math.sqrt(rms) / 32767);
-    return dB
+    return -dB
+
 randomismo = 0
+prev = 0
+decay = 60
+pixels.fill((0,0,0))
+pixels.show()
 while(True):
 #     brightness follows ADC
-    br= int(abs(measure() *1000))
+    br= int(measure(400)*1000 -25)
     
+#     if(br < 20):
 #     print(br)
+   
+    if br > 600:
+        randomismo = random.randrange(-4, 2)
+#         print(randomismo)
     
-    if(br < 900 and br > 45):
-        pixels.brightnessvalue = br
-        if br > 500:
-            randomismo = random.randrange(-5, 3)
-    else:
-        pixels.brightnessvalue = 100
-        
+    pixels.brightnessvalue = 0 if br < 2 else br
+    
         
 #     color oscillates at steps of dVal  
     val = val + (dVal*(1 if dir else -1))
     
-    if val >= 2000:
+    if val >= 1500:
         dir = False
     elif val <= 0:
         dir = True
     
-    r=int(val/80) + randomismo
-    print(randomismo)
-    color = (3*4+r, 4+r, 30-r)
+    r=int(val/90) + randomismo
+#     print(randomismo)
+    color = (3*4+r, 1+r, 30-r)
     rgbw1 = color
     rgbw2 = (56,20+0.3*r, 8-0.3*r)
     
     
 
-#         pixels.set_pixel_line_gradient(0, 255, rgbw1, rgbw2) # display parpadea cuando hay que llegar a muchos pixeles, se nota latencia
+#     pixels.set_pixel_line_gradient(0, 255, rgbw1, rgbw2) # display parpadea cuando hay que llegar a muchos pixeles, se nota latencia
     
     pixels.fill(rgbw1)
 #     hay que investigar porqué el color (aprox) blanco se consigue con (r,g,b)=(94,60,255) en la matriz 16x16
